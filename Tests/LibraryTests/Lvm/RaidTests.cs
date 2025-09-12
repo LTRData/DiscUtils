667c0ad6f5f80bd0ebd82a91f069bbdb5fce5963
LTRData <ol@ltr-data.se>
2025-09-12 15:02:07 +0200

    Linux RAID tests

---
+++ Tests/LibraryTests/Lvm/RaidTests.cs
@@ -0,0 +1,29 @@
+﻿using DiscUtils;
+using DiscUtils.Complete;
+using DiscUtils.Streams;
+using DiscUtils.Vhdx;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LibraryTests.Lvm;
+
+public class RaidTests
+{
+    [Fact]
+    public void RecognizeMirror()
+    {
+        SetupHelper.SetupComplete();
+        using var vhdx = Helpers.Helpers.LoadTestDataFileFromGZipFile("Lvm", "raidtest.vhdx.gz");
+        using var diskImage = new DiskImageFile(vhdx, Ownership.Dispose);
+        using var disk = new Disk([diskImage], Ownership.Dispose);
+        var manager = new VolumeManager(disk);
+        
+        var logicalVolumes = manager.GetLogicalVolumes();
+        
+        Assert.Single(logicalVolumes);
+
+        Assert.Equal("Linux RAID 1 (Mirror)", logicalVolumes[0].TypeAsString);
+
+        Assert.Equal(50331648, logicalVolumes[0].Length);
+    }
+}
