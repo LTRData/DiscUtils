26e1a6d5bc86367f6fe19aa61f0a52eacd363de2
LTRData <ol@ltr-data.se>
2026-04-24 18:30:15 +0200

    Fix for btrfs where chunk tree is internal node

--- Library/DiscUtils.Btrfs/VfsBtrfsFileSystem.cs
+++ Library/DiscUtils.Btrfs/VfsBtrfsFileSystem.cs
@@ -48,58 +48,9 @@ internal sealed class VfsBtrfsFileSystem : VfsReadOnlyFileSystem<DirEntry, File,
         {
             RawStream = stream,
         };
-        foreach (var offset in BtrfsFileSystem.SuperblockOffsets)
-        {
-            if (offset + SuperBlock.Length > stream.Length)
-            {
-                break;
-            }
 
-            stream.Position = offset;
-            var superblockData = stream.ReadExactly(SuperBlock.Length);
-            var superblock = new SuperBlock();
-            superblock.ReadFrom(superblockData);
-
-            if (superblock.Magic != SuperBlock.BtrfsMagic)
-            {
-                throw new IOException("Invalid Superblock Magic");
-            }
-
-            if (Context.SuperBlock == null)
-            {
-                Context.SuperBlock = superblock;
-            }
-            else if (Context.SuperBlock.Generation < superblock.Generation)
-            {
-                Context.SuperBlock = superblock;
-            }
-
-            Context.VerifyChecksum(superblock.Checksum, superblockData.AsSpan(0x20, 0x1000 - 0x20));
-        }
-
-        if (Context.SuperBlock == null)
-        {
-            throw new IOException("No Superblock detected");
-        }
-
-        Context.ChunkTreeRoot = Context.ReadTree(Context.SuperBlock.ChunkRoot, Context.SuperBlock.ChunkRootLevel);
-        Context.RootTreeRoot = Context.ReadTree(Context.SuperBlock.Root, Context.SuperBlock.RootLevel);
-
-        var rootDir = (DirItem)Context.FindKey(Context.SuperBlock.RootDirObjectid, ItemType.DirItem);
-        RootItem fsTreeLocation;
-        if (!options.UseDefaultSubvolume)
-        {
-            fsTreeLocation = (RootItem)Context.FindKey(options.SubvolumeId, ItemType.RootItem);
-        }
-        else
-        {
-            fsTreeLocation = (RootItem)Context.FindKey(rootDir.ChildLocation.ObjectId, rootDir.ChildLocation.ItemType);
-        }
-
-        var rootDirObjectId = fsTreeLocation.RootDirId;
-        Context.FsTrees.Add(rootDir.ChildLocation.ObjectId, Context.ReadTree(fsTreeLocation.ByteNr, fsTreeLocation.Level));
-
-        var dirEntry = new DirEntry(rootDir.ChildLocation.ObjectId, rootDirObjectId);
+        var dirEntry = Context.Initialize();
+        
         RootDirectory = new Directory(dirEntry, Context);
     }
 
