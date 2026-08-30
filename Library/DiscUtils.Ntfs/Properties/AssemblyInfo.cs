6da57ec995a45f170508c826abfb00480dd25b9a
Olof Lagerkvist (LTRData) <ol@ltr-data.se>
2026-08-30 02:45:03 +0200

    Fix NTFS attribute list name offset (#75)

--- Library/DiscUtils.Ntfs/Properties/AssemblyInfo.cs
+++ Library/DiscUtils.Ntfs/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -9,3 +10,4 @@
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("9095333e-08e4-4c13-b556-20b5fdb9db2a")]
+[assembly: InternalsVisibleTo("LibraryTests")]
